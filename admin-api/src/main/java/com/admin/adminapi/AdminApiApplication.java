e5d3475cd5908c379273d1799af9f13d16212435
Karen Oflyan <karen@seguru.io>
2018-03-25 12:18:06 +0300

    added swagger support

--- admin-api/src/main/java/com/admin/adminapi/AdminApiApplication.java
+++ admin-api/src/main/java/com/admin/adminapi/AdminApiApplication.java
@@ -1,14 +1,10 @@
 package com.admin.adminapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@EnableAsync
 public class AdminApiApplication {
 
 	public static void main(String[] args) {
--- admin-api/src/main/java/com/admin/adminapi/Config.java
+++ admin-api/src/main/java/com/admin/adminapi/Config.java
@@ -1,29 +1,31 @@
 package com.admin.adminapi;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @ControllerAdvice(basePackages = "com.admin.adminapi.impl.controller")
+@EntityScan(basePackages = "com.admin.adminapi.impl.dao.entities")
+@EnableSwagger2
 public class Config {
 
     @Bean
@@ -32,6 +34,15 @@ public class Config {
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 
