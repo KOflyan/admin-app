fd694fd644a6bfbb6bc046b7c6e214cdfdf701a2
Karen Oflyan <karen@seguru.io>
2018-05-14 13:48:58 +0300

    typo

--- admin-api/src/main/java/com/admin/adminapi/impl/dao/UserDao.java
+++ admin-api/src/main/java/com/admin/adminapi/impl/dao/UserDao.java
@@ -15,7 +15,6 @@ public class UserDao extends Dao<AbstractUser> {
 
     @Override
     public AbstractUser find(Long id) {
-        System.out.println(id);
         return em.createNamedQuery("User.getById", ExtendedUser.class)
                 .setParameter("id", id)
                 .getSingleResult();
